50b507d0eeec7bcf5a1e682c25c24cda2cf77198
Tomas Flam <tomas.flam@gmail.com>
2012-02-03 09:40:57 +0100

    append inline to publishable objects in newman

--- ella_attachments/newman_admin.py
+++ ella_attachments/newman_admin.py
@@ -1,6 +1,15 @@
+from django.db import models
+
 from ella import newman
+from ella.core.models import Publishable
 
 from ella_attachments.models import Attachment, Type
+
+
+class AttachmentInlineAdmin(newman.NewmanTabularInline):
+    model = Attachment.publishables.through
+    extra = 0
+    suggest_fields = {'attachment': ('name',)}
 
 
 class AttachmentAdmin(newman.NewmanModelAdmin):
@@ -16,5 +25,13 @@ class TypeAdmin(newman.NewmanModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
 
 
+def append_inlines():
+    publishables = ['%s.%s' % (x._meta.app_label, x._meta.object_name)
+                    for x in models.get_models() if issubclass(x, Publishable)]
+    newman.site.append_inline(publishables, AttachmentInlineAdmin)
+
+
 newman.site.register(Attachment, AttachmentAdmin)
 newman.site.register(Type, TypeAdmin)
+
+append_inlines()
